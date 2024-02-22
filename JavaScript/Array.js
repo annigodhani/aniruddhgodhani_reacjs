2877250c226dff69e30e3380e0fb669df3827d8b
annigodhani <aniruddhgodhani@gmail.com>
2024-02-22 12:10:21 +0530

    22/02/24

---
+++ JavaScript/Array.js
@@ -0,0 +1,483 @@
+/* Javascript Array */
+
+let newarry = [1, false, "string"]
+console.log(newarry)
+console.log(newarry.length)
+
+let newarry2 = ["Apple", "Banana", "Watermelon", , "Lichi"]
+console.log(newarry2)
+console.log(newarry2[4])
+console.log(Object.keys(newarry2))
+console.log(newarry2.length)
+
+newarry2.length = 10
+console.log(newarry2)
+console.log(Object.keys(newarry2))
+
+newarry2[0] = "Graps"
+console.log(newarry2)
+
+let newstring = newarry2.toString()
+console.log(newstring)
+console.log(typeof (newstring))
+
+
+// // Array length properties
+// // Accessing the First Array Element
+// // Accessing the Last Array Element
+// // Adding Array Elements
+// // Converting Arrays to Strings
+
+// /*
+// Array indexes start with 0:
+// [0] is the first array element
+// [1] is the second 
+// [2] is the third ...
+// */
+
+
+// /* The length property provides an easy way to append new elements to an array without using the push() method.*/
+
+
+// /*****  Array Methods ********/
+
+// /* Array length properties */
+
+let arry = ["option-1", "gamit", false, 1, "skill", "development"]
+console.log(arry)
+console.log(arry[0])
+console.log(arry[5])
+arry.push("frontend")
+console.log(arry)
+arry.pop()
+arry.pop()
+arry.pop()
+console.log(arry)
+
+arry[0] = "one"
+
+console.log(arry)
+
+// /* The length property provides an easy way to append new elements to an array without using the push() method.*/
+
+
+
+// /* The join() method also joins all array elements into a string. */
+
+{
+    let arry1 = ["option-1", "gamit", false, 1, "skill", "development"]
+    let arry2 = arry1.join("+")
+    console.log(arry2)
+    console.log(typeof (arry2))
+}
+
+// /* Popping in javascript Array */
+
+{
+    const fruits = ["Banana", "Orange", "Apple", "Mango"]
+    document.getElementById("demo").innerHTML = fruits
+
+    document.getElementById("demo-1").innerHTML = fruits.pop()
+
+    console.log(fruits)
+
+    console.log(fruits.length)
+}
+
+// /* The pop() method returns the value that was "popped out" */
+
+{
+    const fruits1 = ["Banana", "Orange", "Apple", "Mango"]
+
+    document.getElementById("demo").innerHTML = fruits1
+
+    document.getElementById("demo-1").innerHTML = fruits1.pop()
+    document.getElementById("demo-2").innerHTML = fruits1.pop()
+    document.getElementById("demo-3").innerHTML = fruits1.pop()
+
+    console.log(fruits1)
+}
+
+
+// /* Pushing in javascript Array */
+
+// /* The push() method adds a new element to an array (at the end) */
+
+{
+    const fruits2 = ["Banana", "Orange", "Apple", "Mango"]
+
+    document.getElementById("demo").innerHTML = fruits2
+
+    fruits2.push("Kiwi")
+
+    document.getElementById("demo-1").innerHTML = fruits2
+
+    console.log(fruits2)
+}
+
+{
+    const fruits3 = ["Banana", "Orange", "Apple", "Mango"]
+
+    document.getElementById("demo").innerHTML = fruits3.push("Kiwi")
+
+    document.getElementById("demo-1").innerHTML = fruits3.length
+}
+
+// /* Shifting Elements */
+
+// /* JavaScript Array shift() */
+
+// /* The shift() method removes the first array element and "shifts" all other elements to a lower index */
+
+{
+    let arry3 = ["option-1", "gamit", false, 1, "skill", "development"]
+    let arry4 = arry3.shift()
+    let arry5 = arry3.shift()
+    console.log(arry4)
+    console.log(arry3)
+    console.log(arry5)
+}
+
+// /*  The shift() method returns the value that was "shifted out" */
+
+{
+    const fruits4 = ["Banana", "Orange", "Apple", "Mango"]
+    console.log(fruits4)
+    document.getElementById("demo").innerHTML = fruits4.shift()
+    document.getElementById("demo-1").innerHTML = fruits4
+    document.getElementById("demo-2").innerHTML = fruits4
+    document.getElementById("demo-3").innerHTML = fruits4
+}
+
+// /*  JavaScript Array unshift() */
+// /* The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements */
+
+{
+    let arryy = ["option-1", "gamit", false, 1, "skill", "development"]
+    arryy.unshift("OneAuthor")
+    console.log(arryy)
+}
+
+// /* The unshift() method returns the new array length */
+
+{
+    const fruits5 = ["Banana", "Orange", "Apple", "Mango"]
+    document.getElementById("demo").innerHTML = fruits5.unshift("Lemon")
+    document.getElementById("demo-1").innerHTML = fruits5
+}
+
+// /*  Changing Elements */
+
+{
+    const fruits6 = ["Banana", "Orange", "Apple", "Mango"]
+    console.log(fruits6)
+
+    fruits6[10] = "Kiwi"
+    console.log(fruits6)
+}
+
+// /* JavaScript Array add element */
+
+{
+    const fruits7 = ["Banana", "Orange", "Apple", "Mango"]
+    document.getElementById("demo").innerHTML = fruits7
+
+    fruits7[fruits7.length] = "Kiwi"
+    document.getElementById("demo-1").innerHTML = fruits7
+    console.log(fruits7);
+}
+
+// /* JavaScript Array delete() */
+
+// /* Array elements can be deleted using the JavaScript operator delete. */
+
+// /* Using delete leaves undefined holes in the array. */
+
+// /* Use pop() or shift() instead. */
+
+{
+    let arrya = ["option-1", "gamit", false, 1, "skill", "development"]
+    console.log(arrya)
+    console.log(arrya.length)
+    delete arry[4]
+    console.log(arrya)
+    console.log(arrya.length)
+
+    arrya.length = 20
+    console.log(arrya)
+
+    arrya[16] = "skillQode"
+    console.log(arrya)
+}
+
+// /* Merging (Concatenating) Arrays */
+
+{
+
+    let arry11 = ["javascript", "c", "c++", "python", "html", "css"]
+    let arry22 = ["kotlin", "dart", "nodejs", "php", "wordpress"]
+    let arry33 = ["boostrap", "tailwind", "reactjs", "nextjs"]
+
+    let arry44 = arry11.concat(arry33, arry22)
+
+    console.log(arry44)
+
+}
+
+// /* The concat() method does not change the existing arrays. It always returns a new array */
+
+// /* Splicing and Slicing Arrays */
+
+// /* JavaScript Array splice() */
+
+// // splice(start)
+// // splice(start, deleteCount)
+// // splice(start, deleteCount, item1)
+// // splice(start, deleteCount, item1, item2)
+// // splice(start, deleteCount, item1, item2, /* …, */ itemN)
+
+{
+    let ar = ["Ani", "Vijay", "Kartik", "Hari", "Varun", "Chirag"]
+    console.log(ar)
+
+    let arr = ar.splice(3, 3, "Gopal", "Vivek")
+    console.log(arr)
+    console.log(ar)
+
+}
+
+// /* JavaScript Array slice() */
+
+// // slice()
+// // slice(start)
+// // slice(start, end)
+
+{
+    let a1 = [45, 89, 21, 2, 57, 23]
+    console.log(a1)
+
+    let a2 = a1.slice(0, 3)
+    console.log(a2)
+    console.log(a1)
+}
+
+// /*  Array.isArray */
+
+{
+    console.log(Array.isArray(17))
+}
+
+/*  javascript Sorting Array */
+
+{
+    let ary1 = ["Ani", "Kartik", "Vijay", "Hari", "Varun", "Gopal"]
+    console.log(ary1)
+
+    let ary2 = ary1.sort()
+    console.log(ary2)
+}
+
+// /*  Reversing array */
+
+{
+    let arr1 = ["Gopal", "Vijay", "Janak", "Anii", "Nimesh", "Chirag"]
+    console.log(arr1)
+
+    let arr2 = arr1.reverse()
+    console.log(arr2)
+}
+
+// /*  Number Sort */
+
+{
+    let ay1 = [189, 45, 56, 128, 10, 150]
+    console.log(ay1)
+
+    let numbersorted = ay1.sort((a, b) => a - b)
+    console.log(numbersorted)
+}
+
+// /*  javascript array map() */
+
+{
+    const number1 = [56, 798, 345, 35]
+    console.log(number1)
+    const number2 = number1.map(myFunction)
+
+    const number3 = number2.sort((a, b) => a - b)
+
+    console.log(number2)
+
+    function myFunction(value) {
+        return value * 2
+    }
+}
+
+/* JavaScript Array slice() */
+
+/*  javascript Array flatMap() */
+
+{
+
+    let flatmap = [1, 2, 3, 4]
+
+    let flatmap1 = flatmap.flatMap((num) => num === 3 ? [1, 1] : 2)
+
+    console.log(flatmap1)
+}
+
+/* Javascript Array flat() */
+
+{
+    let flatarray = [10, 20, [22, 33, [44, 55, [66, 77]]]]
+    let flatmap2 = flatarray.flat(3)
+    console.log(flatmap2)
+}
+
+// /* javascript array filter() */
+
+{
+
+    let filterarry = ["Anni", "Nikunj", "Vijay", "Gopal", "Hari", "Janak"]
+    let filterarry2 = filterarry.filter((word) => word.length < 5)
+    console.log(filterarry2)
+
+    let filterarry3 = filterarry2.sort()
+    console.log(filterarry3)
+
+}
+
+// /*  javascript array fill method */
+
+{
+    let fillarray  = [46 , 89 , 101 , 122 , 45]
+    console.log(fillarray)
+    let fillarray2 = fillarray.fill("None" , 1)
+    console.log(fillarray2)
+}
+
+// /* javascript array copywithin() method */
+
+{
+    const alfa = ["A", "B", "C", "D", "E", "F"]
+    const newcopy = alfa.copyWithin(0, 3, 4)
+    console.log(newcopy)
+
+    console.log([1, 2, 3, 4, 5 , 6].copyWithin(0, 2));
+
+    console.log([1, 2, 3, 4, 5 , 6 , 7].copyWithin(2, 4 , 5));
+
+    console.log([7,6,5,4,3,2,1].copyWithin(2, 3));
+    // [1, 2, 3, 3, 4]
+}
+
+/* javascript valueof() method */
+
+{
+    const fruitss = ["Banana", "Orange", "Lemon", "Apple", "Mango", "watermelon", "kiwi", "Olive"]
+    const newfruits = fruitss.valueOf()
+   document.write(newfruits)
+}
+
+// /*  javascript flat() method */
+
+// {
+//     const numm = [1, 2, 3, [4, [5, 6, [7, 8, 9]]]]
+//     const newarray1 = numm.flat()
+//     const newarray2 = numm.flat(2)
+//     const newarray3 = numm.flat(3)
+//     const newarray4 = numm.flat(4)
+//     console.log(newarray1)
+//     console.log(newarray2)
+//     console.log(newarray3)
+//     console.log(newarray4)
+// }
+
+ /*  JavaScript Multidimensional Array */
+ 
+ /* A multidimensional array is an array that contains another array. */
+ 
+ {
+    // multidimensional array
+    const data = [[1, 2, 3], [1, 3, 4], [4, 5, 6]]
+    console.log(data)
+}
+
+/* Access Elements of an Array */
+
+{
+   const data = [[1, 2, 3], [1, 9, 4], [4, 5, 6]]
+   let data1 = data[1][1]
+   console.log(data1)
+}
+
+/* Add an Element to a Multidimensional Array */
+
+{
+    // Adding Element to the Outer Array
+
+    let studentsData = [['Anni', 22], ['Gopal', 25]]
+    studentsData.push(['Kartik', 24])
+
+    let data = studentsData[1][1] 
+
+    console.log(studentsData)
+    console.log(data)
+}
+
+{
+    // Adding Element to the Inner Array
+    {
+        // using index notation
+        let studentsData = [['Vijay', 24], ['Janak', 26],]
+        studentsData[1][2] = 'hello'
+    
+        console.log(studentsData)
+    }
+
+    {
+        // using push()
+        let studentsData = [['Chirag', 28], ['Hari', 24],]
+        studentsData[1].push('hello')
+
+        console.log(studentsData)
+    }
+      
+    {
+        // the Array's splice() method to add an element at a specified index.
+
+        let studentsData = [['Kishan', 23], ['Nikunj', 25]]
+
+        // adding element at 1 index
+        studentsData.splice(0, 2 , ["peter"  , 58])
+
+        console.log(studentsData)
+    }
+
+    /*  Remove an Element from a Multidimensional Array */
+
+    {
+        // remove the array element from outer array
+        let studentsData = [['Vishal', 24], ['Ekta', 23]]
+        studentsData.pop()
+
+        console.log(studentsData)
+    }
+
+    {
+        // remove the element from the inner array
+        let studentsData = [['Jemisha', 22], ['Saki', 23]]
+        studentsData[1].pop()
+
+        console.log(studentsData)   // [["Jemisha" , 22], ["Saki" , 23]]
+    }
+
+    {
+       // the splice() method to remove an element at a specified index.
+        let studentsData = [['Vishakha', 25], ['Nidhi', 24]]
+
+        // removing 1 index array item
+        studentsData.splice(0,1)
+        console.log(studentsData)
+    }
+}
---
+++ JavaScript/Pract.js
@@ -0,0 +1,524 @@
+// primitive string 
+
+let S1 = "Hello"
+let S2 = 'World'
+let S3 = `Hello World`
+
+console.log(S1)
+console.log(S2)
+console.log(S3)
+
+
+// string as object 
+
+let S4 = new String("Aniruddh")
+
+console.log(S4)
+
+let S5 = S4.split("")
+console.log(S5)
+
+//  String length.
+
+let S6 = "WHAT ARE MOST SIMPLE."
+console.log(S6.length)
+
+
+//  String charAt() 
+
+let S7 = "MOST Populer FOOd."
+let S8 = S7.charAt(6)
+console.log(S8)
+
+
+// String slice() return New String 
+
+let String9 = "Noty Boy."
+let String10 = String9.slice(6)
+let String11 = String9.slice(-10)
+let String12 = String9.slice(-5, -12)
+let String13 = String9.slice(6, 8)
+let String14 = String9.slice(-3, 14)
+let String15 = String9.slice(4, -9)
+
+console.log(String10)
+console.log(String11)
+console.log(String13)
+console.log(String14)
+console.log(String15)
+
+// String substring() return New String 
+
+let String16 = "Happy Girl."
+let String17 = String16.substring(10)
+let String18 = String16.slice(12)
+let String19 = String16.substring(-10)
+let String20 = String16.slice(-13)
+let String21 = String16.substring(10, 30)
+let String22 = String16.substring(2, -20)
+let String23 = String16.substring(-10, 8)
+
+console.log(String17)
+console.log(String18)
+console.log(String19)
+console.log(String20)
+console.log(String21)
+console.log(String22)
+console.log(String23)
+
+// String replace()
+
+let String24 = "WHAT ARE MOST SIMPLE Guy's."
+let String25 = String24.replace("MOST", "The")
+
+console.log(String25)
+
+// String replaceAll()
+
+let String27 = "Simple Guy's Is Very Simple."
+let String28 = String27.replaceAll("Simple", "Boring")
+console.log(String28)
+
+
+// String toUpperCase()
+
+let String30 = "aniruddh godhani"
+let String31 = String30.toUpperCase()
+console.log(String31)
+
+
+//  String toLowerCase()
+
+let String32 = "ANIRUDDH GODHANI"
+let String33 = String32.toLowerCase()
+console.log(String33)
+
+
+//  String concat()
+
+let String34 = "Hello"
+let String35 = "How Are You!"
+let String36 = String34.concat(" " + String35)
+console.log(String36)
+
+
+// String trim()
+
+let String38 = "  This is most common lecture!  "
+console.log(String38.length)
+
+let String39 = String38.trim()
+console.log(String39)
+console.log(String39.length)
+
+
+//  String trimStart()
+
+let String40 = "  This is most common lecture!  "
+console.log(String40.length)
+
+let String41 = String40.trimStart()
+console.log(String41)
+console.log(String41.length)
+
+
+//  String trimEnd() 
+
+let String42 = "   This is most common lecture!   "
+console.log(String42)
+console.log(String42.length)
+
+let String43 = String42.trimEnd()
+console.log(String43)
+console.log(String43.length)
+
+
+//  String padStart() 
+
+let String44 = "This is Common langauage!"
+console.log(String44)
+console.log(String44.length)
+
+let String45 = String44.padStart(5, "1")
+console.log(String45)
+
+let Number1 = "500"
+let Number2 = Number1.padStart(3, 0)
+console.log(Number2)
+
+
+// String padEnd() 
+
+let Number3 = "2002"
+let Number4 = Number1.padEnd(4, 0)
+console.log(Number4)
+console.log(typeof (Number4)) 
+
+let Number5 = parseFloat(Number4)
+console.log(Number5)
+console.log(typeof (Number5))
+
+
+//  String charCodeAt() 
+
+let String46 = "This is most CoMmon!"
+let String47 = String46.charCodeAt(15)
+console.log(String47)
+
+
+// String split() 
+
+let String48 = "Aniruddh Godhani."
+console.log(String48)
+let String49 = String48.split("")
+console.log(String49)
+
+
+//  String includes() , true and false 
+
+let String50 = "Aniruddh Godhani"
+console.log(String50)
+let String51 = String50.includes("a")
+console.log(String51)
+
+//Q-1 Declare an empty array
+
+let newarry = []
+console.log(newarry)
+
+// Q-2 Declare an array with more than 5 number of elements
+
+let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(a)
+
+// Q-3 Find the length of your array
+
+let v = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log('length is' + ' ' + v.length)
+
+// Q-4 Get the first item, the middle item and the last item of the array
+
+let A = [1, 2, 3, 4, 5, 6, 7]
+let G = A[0]
+let V = A[Math.floor(A.length / 2)]
+let Ka = A[A.length - 1]
+
+console.log("G :", G)
+console.log("V :", V)
+console.log("Ka :", Ka)
+
+// Q-5 Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+
+let N = [1, 'apple', true, { key: 'value' }, [2, 4, 6], null, undefined]
+let J = N.length
+
+console.log("N :", N)
+console.log("Length :", J)
+
+// Q-6 Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+// Print the array using console.log()
+// Print the number of companies in the array
+// Print the first company, middle and last company
+// Print out each company
+
+let Companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+console.log("Array :", Companies)
+console.log("Number of companies :", Companies.length)
+
+let first = Companies[0]
+let middle = Companies[Math.floor(Companies.length / 2)]
+let last = Companies[Companies.length - 1]
+
+console.log("First :", first)
+console.log("Middle :", middle)
+console.log("Last :", last)
+
+// Q-7 Change each company name to uppercase one by one and print them out
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+console.log("Company Names in Uppercase:")
+itCompanies.forEach((company, index) => {
+    let uppercasedCompany = company.toUpperCase()
+    console.log(`Company ${index + 1}: ${uppercasedCompany}`)
+})
+
+let sentence = itCompanies.join(', ') + ' are big IT companies.'
+console.log("Array as a Sentence:", sentence)
+
+// Q-8 Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+// Filter out companies which have more than one 'o' without the filter method
+
+let itCompanie = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+function checkAndFilter(companyName) {
+    let found = false
+    let filteredCompanie = []
+
+    for (let i = 0; i < itCompanie.length; i++) {
+        let company = itCompanies[i]
+
+        if (company.toLowerCase() === companyName.toLowerCase()) {
+            found = true
+        }
+
+        let count = 0
+        for (let j = 0; j < company.length; j++) {
+            if (company[j].toLowerCase() === 'o') {
+                count++
+            }
+        }
+
+        if (count <= 1) {
+            filteredCompanie.push(company)
+        }
+    }
+
+    if (found) {
+        return `${companyName} exists in the array.`
+    }
+
+    else {
+        return `${companyName} is not found in the array.`
+    }
+}
+
+let companyNameToCheck = 'Microsoft'
+let result = checkAndFilter(companyNameToCheck)
+console.log(result)
+
+console.log("Filtered Companies:", companyNameToCheck)
+
+// Q-9 Sort the array using sort() method
+
+{
+    let ary1 = ["Ani", "Kartik", "Vijay", "Hari", "Varun", "Gopal"]
+    console.log(ary1)
+
+    let ary2 = ary1.sort()
+    console.log(ary2)
+}
+
+// Q-10 Reverse the array using reverse() method
+
+{
+    let arr1 = ["Gopal", "Vijay", "Janak", "Anii", "Nimesh", "Chirag"]
+    console.log(arr1)
+
+    let arr2 = arr1.reverse()
+    console.log(arr2)
+}
+
+// Q-11 Slice out the first 3 companies from the array
+
+let name1 = ["Gopal", "Vijay", "Janak", "Anii", "Nimesh", "Chirag"]
+let K = 3
+console.log(name1)
+const result1 = name1.slice(0, 3)
+console.log(result1)
+
+// Q-12 Slice out the last 3 companies from the array
+
+let name2 = ["Gopal", "Vijay", "Janak", "Anii", "Nimesh", "Chirag"]
+console.log(name2)
+const result2 = name2.slice(-3)
+console.log(result2)
+
+// Q-13 Slice out the middle IT company or companies from the array
+// Remove the first IT company from the array
+// Remove the middle IT company or companies from the array
+// Remove the last IT company from the array
+// Remove all IT companies
+
+let itCompani = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+function checkCompanyExists(companyName) {
+    if (itCompanies.includes(companyName)) {
+        return `${companyName} exists in the array.`
+    }
+    else {
+        return `${companyName} is not found in the array.`
+    }
+}
+
+let companyName = 'Microsoft'
+let result3 = checkCompanyExists(companyName)
+console.log(result3)
+
+let filteredCompanies = []
+for (let i = 0; i < itCompanies.length; i++) {
+    let company = itCompani[i]
+    let count = 0
+    for (let j = 0; j < company.length; j++) {
+        if (company[j].toLowerCase() === 'o') {
+            count++
+        }
+    }
+
+    if (count <= 1) {
+        filteredCompanies.push(company)
+    }
+}
+console.log("Filtered Companies:", filteredCompanies)
+
+let middleIndex = Math.floor(itCompanies.length / 2)
+let middleCompanies = itCompanies.slice(middleIndex, middleIndex + 1)
+console.log("Middle IT Company:", middleCompanies)
+
+itCompani.shift()
+console.log("Array after removing the first IT company:", itCompani)
+
+itCompani.splice(middleIndex, 1)
+console.log("Array after removing the middle IT company:", itCompani)
+
+itCompani.pop()
+console.log("Array after removing the last IT company:", itCompani)
+
+itCompani = []
+console.log("Array after removing all IT compani:", itCompani)
+
+
+
+
+// 1. Write a function that returns the square of a number 1 to 20
+
+{
+    function Square(number) {
+        return number * number
+    }
+
+    for (let i = 1; i <= 20; i++) {
+        console.log(Square(i))
+    }
+
+}
+
+// 2. Write a function to convert Celsius to Fahrenheit
+
+{
+    function Celsius(v) {
+        return (v * 9 / 5) + 32
+    }
+    let result4 = Celsius(22)
+    console.log(result4)
+}
+
+// 3. Write a function to find the area of a given Rectangle
+
+{
+    function rect(l, w) {
+        return l * w
+    }
+    console.log(rect(20, 13))
+}
+
+// 4. Write a function to reverse a number
+
+{
+    function rever() {
+        for (let j = 10; j >= 1; j--)
+            console.log(j)
+    }
+}
+rever()
+
+// 5. Count the number of Vowels in the String
+
+// {
+//     function k(stg) {
+//         let vowel = 'aAeEiIoOuU'
+//     }
+// }
+
+function countVowels(str) {
+
+
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    var count = 0
+
+    for (var i = 0; i < str.length; i++) {
+
+        if (vowels.indexOf(str[i]) !== -1) {
+            count++
+        }
+    }
+
+    return count
+}
+
+var inputString = "Hello, Aniruddh Godhani"
+var resultt = countVowels(inputString)
+
+console.log("Number of vowels :", resultt)
+
+
+// 6. Write a function to calculate simple interest based on the principal amount
+
+{
+    function simple(p, r, t) {
+        let interest = (p * r * t) / 100
+        return interest
+    }
+    console.log(simple(3000, 4, 3))
+}
+
+// 7. Write a function to generate a random number
+
+{
+    function random(m, n) {
+        return Math.floor(Math.random() * (m - n + 1)) + n
+    }
+    console.log(random(1, 100))
+}
+
+// 8. Write a JavaScript program to display the current day and time in the following format.
+
+{
+    function date() {
+        let now = new Date()
+        console.log(now)
+        const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        console.log(weekday[now.getDay()])
+        let day = now.getDay()
+        console.log(day)
+        let hours = now.getHours()
+        let minutes = now.getMinutes()
+
+        console.log(day, hours, ":", minutes)
+    }
+    date()
+}
+
+// 9. Write a JavaScript function that returns a passed string with letters in alphabetical order.
+
+{
+    function order(s) {
+        return s.split('').sort().join('')
+    }
+    console.log("Aniruddh = ", order("Aniruddh Godhani"))
+}
+
+// sorting with word first letter in order form
+
+{
+
+    var word = ["Gopal", "Vijay", "Anni", "Hari", "Chirag", "Kartik", "Janak" , "Dhruvil"]
+
+    word.sort(function (a, v) {
+        var LetterA = a.charAt(0)
+        var LetterV = v.charAt(0)
+
+        if (LetterA < LetterV) {
+            return -1
+        }
+    })
+
+    console.log(word)
+}
+
+// 10. Write a JavaScript conditional statement to find the sign of the product of three numbers. Display an alert box with the specified sign.
+
+
